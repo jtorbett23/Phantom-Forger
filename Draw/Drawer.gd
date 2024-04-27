12d135ece29a356a7112a7dc39827f69d23a5cba
jtorbett23 <56300912+jtorbett23@users.noreply.github.com>
2024-04-28 00:02:55 +0100

    more efficient drawing

--- Draw/Drawer.gd
+++ Draw/Drawer.gd
@@ -1,28 +1,38 @@
 extends Sprite2D
 
-var radius : int = 10
+var radius : int = 20
 var colour : Color = Color.BLACK
-var draw_datas : Array = []
+var draw_datas : Array[Dictionary] = []
 var enabled : bool = true
+var redraw_limit : int = 200
 
-#TODO: Investigate a more peformant solution to drawing
+@onready var viewport : SubViewport = self.get_parent()
 
-func _draw():
+#TODO: track redraws to undo, add eraser, add clear 
+
+func _draw() -> void:
 	if draw_datas.size() > 0:
-		for data in draw_datas:
+		for i in range(0, redraw_limit):
+			if i > draw_datas.size() - 1:
+				return
+			var data : Dictionary = draw_datas[draw_datas.size() - 1 - i]
 			draw_circle(data.pos, data.radius, data.colour)
+
+func draw_canvas_redrawing(pos : Vector2i):
+		var draw_data = {"pos": pos, "radius": radius, "colour" : colour}
+		if not draw_data in draw_datas:
+			draw_datas.append(draw_data)
+			queue_redraw()
+			if draw_datas.size() % (redraw_limit) and draw_data.size() > 0:
+				print(viewport.name)
+				var new_image : Image = viewport.get_texture().get_image()
+				self.texture = ImageTexture.create_from_image(new_image)
 
 func _process(_delta) -> void:
 	if enabled:
-		var mouse_pos : Vector2 = get_global_mouse_position()
-		if is_on_canvas(mouse_pos) and Input.is_action_pressed("draw"):
-			var draw_data = {"pos": mouse_pos - self.position, "radius": radius, "colour" : colour}
-			if not draw_data in draw_datas:
-				draw_datas.append(draw_data)
-				print(draw_datas.size())
-				queue_redraw()
-	
-
+		var pos : Vector2i = get_local_mouse_position()
+		if is_on_canvas(pos) and Input.is_action_pressed("draw"):
+			draw_canvas_redrawing(pos)
 
 func is_on_canvas(mouse_pos : Vector2) -> bool:
 	var min_pos : Vector2 = self.position
