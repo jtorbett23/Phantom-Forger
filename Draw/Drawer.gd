9b3fa691a6f1650ef8266e2bd0f38ef4980d5b8e
jtorbett23 <56300912+jtorbett23@users.noreply.github.com>
2024-04-28 01:00:49 +0100

    finish clear and undo

--- Draw/Drawer.gd
+++ Draw/Drawer.gd
@@ -3,10 +3,12 @@ extends Sprite2D
 var radius : int = 20
 var colour : Color = Color.BLACK
 var draw_datas : Array[Dictionary] = []
+var prev_draw_datas : Array[Array] = []
 var enabled : bool = true
 var redraw_limit : int = 200
 
 @onready var viewport : SubViewport = self.get_parent()
+@onready var original_canvas : Image = self.texture.get_image()
 
 #TODO: track redraws to undo, add eraser, add clear 
 
@@ -16,15 +18,16 @@ func _draw() -> void:
 			if i > draw_datas.size() - 1:
 				return
 			var data : Dictionary = draw_datas[draw_datas.size() - 1 - i]
-			draw_circle(data.pos, data.radius, data.colour)
+			if(data.has("pos")):
+				draw_circle(data.pos, data.radius, data.colour)
 
 func draw_canvas_redrawing(pos : Vector2i):
 		var draw_data = {"pos": pos, "radius": radius, "colour" : colour}
 		if not draw_data in draw_datas:
 			draw_datas.append(draw_data)
 			queue_redraw()
-			if draw_datas.size() % (redraw_limit) and draw_data.size() > 0:
-				print(viewport.name)
+			if (draw_datas.size() % redraw_limit) == 0 and draw_datas.size() >= redraw_limit:
+				draw_datas[draw_datas.size() -1]["image"] = self.texture.get_image()
 				var new_image : Image = viewport.get_texture().get_image()
 				self.texture = ImageTexture.create_from_image(new_image)
 
@@ -33,6 +36,27 @@ func _process(_delta) -> void:
 		var pos : Vector2i = get_local_mouse_position()
 		if is_on_canvas(pos) and Input.is_action_pressed("draw"):
 			draw_canvas_redrawing(pos)
+		elif Input.is_action_pressed("undo"):
+			if draw_datas.size() > 0:
+				var data = draw_datas.pop_back()
+				if data and data.has("image"):
+					if draw_datas.size() == 0 and prev_draw_datas.size() > 0:
+						draw_datas = prev_draw_datas.pop_back()
+					else:
+						self.texture = ImageTexture.create_from_image(data["image"])
+				queue_redraw()
+			elif prev_draw_datas.size() > 0:
+				draw_datas = prev_draw_datas.pop_back()
+				var data = draw_datas[draw_datas.size() - 1]
+				if data and data.has("image"):
+					self.texture = ImageTexture.create_from_image(data["image"])
+				queue_redraw()
+
+		elif Input.is_action_just_pressed("clear"):
+			prev_draw_datas.append(draw_datas)
+			draw_datas = [{"image": original_canvas}]
+			self.texture = ImageTexture.create_from_image(original_canvas)
+			# queue_redraw()
 
 func is_on_canvas(mouse_pos : Vector2) -> bool:
 	var min_pos : Vector2 = self.position
