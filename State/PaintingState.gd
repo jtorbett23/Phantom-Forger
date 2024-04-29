d65e5e3a340dd7ff6994984b31d8ba1222a0631b
jtorbett23 <56300912+jtorbett23@users.noreply.github.com>
2024-04-29 01:41:44 +0100

    use string

--- State/PaintingState.gd
+++ State/PaintingState.gd
@@ -2,10 +2,10 @@ extends RefCounted
 
 class_name PaintingState 
 
-# static var art_paths : Array[String] = ["res://assets/art/paintings/rabbit.png", "res://assets/art/paintings/sea-star.png", "res://assets/art/paintings/bunny-slippers.png", "res://assets/art/paintings/tortoise.png", "res://assets/art/paintings/axolotl.png", "res://assets/art/paintings/whale-tail.png", "res://assets/art/paintings/pig.png", "res://assets/art/paintings/koala.png"]
+static var art_paths : Array[String] = ["res://assets/art/paintings/rabbit.png", "res://assets/art/paintings/sea-star.png", "res://assets/art/paintings/bunny-slippers.png", "res://assets/art/paintings/tortoise.png", "res://assets/art/paintings/axolotl.png", "res://assets/art/paintings/whale-tail.png", "res://assets/art/paintings/pig.png", "res://assets/art/paintings/koala.png"]
 
-# TODO: export art paths prior 
-static var art_paths : Array[String] = AssetsHelper.get_file_paths(AssetsHelper.paintings_folder_path, "png")
+# TODO: export art paths prior  via AssetHelper
+# static var art_paths : Array[String] = AssetsHelper.get_file_paths(AssetsHelper.paintings_folder_path, "png")
 static var free_art_paths : Array[String] = art_paths.duplicate()
 static var count : int = 0
 
